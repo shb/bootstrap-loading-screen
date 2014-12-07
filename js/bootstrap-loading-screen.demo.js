b3f9586a0acb43f8253e8088e39884bea922efa0
Simone Cociancich <simone.coch@tin.it>
2014-12-07 17:38:44 +0100

    Polished up the appalling angular syntax

--- js/bootstrap-loading-screen.demo.js
+++ js/bootstrap-loading-screen.demo.js
@@ -20,69 +20,72 @@
 		{ name:"Yeti",     href:"//maxcdn.bootstrapcdn.com/bootswatch/3.3.0/yeti/bootstrap.min.css" }
 	]);
 
-	demo.controller('HeadController', [
-		'$rootScope', '$scope',
-		'Stylesheets',
-		function ($root, $scope, styles)
-		{
-			$root.styles = styles;
-			var si = Math.floor(Math.random() * styles.length);
-			$root.style = {
-				index: si,
-				name: styles[si].name,
-				href: styles[si].href
-			};
-			$root.$watch('style.index', function (val, old) {
-				if (val != old) {
-					$('body > *').fadeOut('fast').promise().done(function ()
-					{
-						$root.style.name = styles[val].name;
-						$root.style.href = styles[val].href;
-						$root.$apply('style');
-						$('body > *').fadeIn('slow');
-					});
-				}
-			});
-
-			$root.icons = [
-				{ name:"Circle",  glyph:"circle-o-notch" },
-				{ name:"Gear",    glyph:"cog" },
-				{ name:"Arrows",  glyph:"refresh" },
-				{ name:"Spinner", glyph:"spinner" }
-			];
-			$root.sizes = [
-				{ icon:'lg', title:'h4' },
-				{ icon:'2x', title:'h4' },
-				{ icon:'3x', title:'h3' },
-				{ icon:'4x', title:'h2' },
-				{ icon:'5x', title:'h1' }
-			];
-			var ii = Math.floor(Math.random() * $root.icons.length);
-			var is = Math.floor(Math.random() * $root.sizes.length-1)+1;
-			$root.icon = {
-				index: ii,
-				name: $root.icons[ii].name,
-				glyph: $root.icons[ii].glyph,
-				size: is,
-				blur: Math.floor(Math.random() * 5)
-			};
-			$root.$watch('icon.index', function (val, old) {
-				if (val != old) {
-					$root.icon.name = $root.icons[val].name;
-					$root.icon.glyph = $root.icons[val].glyph;
-				}
-			});
-		}
+	demo.constant('Icons', [
+		{ name:"Circle",  glyph:"circle-o-notch" },
+		{ name:"Gear",    glyph:"cog" },
+		{ name:"Arrows",  glyph:"refresh" },
+		{ name:"Spinner", glyph:"spinner" }
 	]);
 
-	demo.controller('ConfiguratorController', [
-		'$rootScope', '$scope',
-		function ($root, $scope)
-		{
-			/*$scope.styles = $root.styles;
-			$scope.style = $root.style;*/
-		}
+	demo.constant('Sizes', [
+		{ icon:'lg', title:'h4' },
+		{ icon:'2x', title:'h4' },
+		{ icon:'3x', title:'h3' },
+		{ icon:'4x', title:'h2' },
+		{ icon:'5x', title:'h1' }
 	]);
+
+	HeadCntl.$inject = [
+		'$rootScope', '$scope',
+		'Stylesheets', 'Icons', 'Sizes'
+	];
+	demo.controller('HeadController', HeadCntl);
+	function HeadCntl (
+		$root, $scope,
+		styles, icons, sizes
+	) {
+		$root.styles = styles;
+		var si = Math.floor(Math.random() * styles.length);
+		$root.style = {
+			index: si,
+			name: styles[si].name,
+			href: styles[si].href
+		};
+
+		$root.$watch('style.index', watchStyleIndex);
+		function watchStyleIndex (val, old) {
+			if (val != old) {
+				$('body > *').fadeOut('fast').promise().done(function ()
+				{
+					$root.style.name = styles[val].name;
+					$root.style.href = styles[val].href;
+					$root.$apply('style');
+					$('body > *').fadeIn('slow');
+				});
+			}
+		}
+
+		$root.icons = icons;
+		$root.sizes = sizes;
+
+		var is = Math.floor(Math.random() * $root.sizes.length-1)+1;
+		var ii = Math.floor(Math.random() * $root.icons.length);
+		$root.icon = {
+			index: ii,
+			name: $root.icons[ii].name,
+			glyph: $root.icons[ii].glyph,
+			size: is,
+			blur: Math.floor(Math.random() * 5)
+		};
+
+		$root.$watch('icon.index', watchIconIndex);
+		function watchIconIndex (val, old) {
+			if (val != old) {
+				$root.icon.name = $root.icons[val].name;
+				$root.icon.glyph = $root.icons[val].glyph;
+			}
+		}
+	}
 
 })(angular);
 
